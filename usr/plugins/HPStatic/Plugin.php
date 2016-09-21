7766eb6d987bdde0ecec0991f7139305498557db
lgl <gl.lei@qq.com>
2016-09-22 00:45:22 +0800

    已经可以创建表，按还不能cache，原因未知。同时走不到end这个挂载点。

--- usr/plugins/HPStatic/Plugin.php
+++ usr/plugins/HPStatic/Plugin.php
@@ -19,17 +19,18 @@ class HPStatic_Plugin implements Typecho_Plugin_Interface
         $prefix = $installDb->getPrefix();
         $cacheTable = $prefix. 'hpstatic_cache';
         try {
-            $installDb->query("CREATE TABLE `$cacheTable.`(
+            $sql = "CREATE TABLE IF NOT EXISTS `$cacheTable`(
             `hash`    char(32)  NOT NULL,
             `cache`   Blob      NOT NULL,
             `timestamp` timestamp  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
             PRIMARY KEY `hash` (`hash`) 
         ) ENGINE=MyISAM DEFAULT CHARSET=utf8 \n
         PARTITION BY KEY(hash) \n
-        PARTITIONS 64");
+        PARTITIONS 64";
+            $installDb->query($sql);
         } catch (Typecho_Db_Exception $e) {
-            $code = $e->getCode();
-            throw new Typecho_Plugin_Exception('数据表建立失败，插件启用失败。错误号：'.$code);
+            throw new Typecho_Plugin_Exception('数据表建立失败，插件启用失败。错误信息：'.$sql);
+
         }
 
         Typecho_Plugin::factory('index.php')->begin = array('HPStatic_Plugin', 'getCache');
@@ -56,10 +57,81 @@ class HPStatic_Plugin implements Typecho_Plugin_Interface
     }
 
     public static function getCache(){
+        $req = Typecho_Request::getInstance();
+        $url = $req->getRequestUri();
+
+
+        if(!$req->isGet()){ #仅处理GET请求
+            return;
+        }
+        if(strstr($url,'/action/') !== false ){
+            #排除action接口,这个一般是特殊接口,所以不需要缓存
+            return;
+        }
+
+        $db = Typecho_Db::get();
+
+        $hash = md5($url);
+        //die($hash);
+        $query = $db->select('hash','cache','unix_timestamp(timestamp) as timestamp')
+            ->from('table.hpstatic_cache')
+            ->where('hash = ?',$hash)
+            ->limit(1);
+        $result = $db->fetchAll($query);
+
+        @$settings = Helper::options()->plugin('HPStatic');
+        if(!$settings) die('未开启Typecho插件，无法获取超时时间');
+
+
+        $cache_timeout = intval($settings->cache_timeout);
+        if($cache_timeout <= 0){
+            $cache_timeout = 60; //1min
+        }
+
+        if(count($result)>0 && isset($result[0]['timestamp'])){
+            $row = $result[0];
+
+            $last_time = $row['timestamp'];
+
+            if(intval(time()) - intval($last_time) <= $cache_timeout){
+                $html = gzuncompress($row['cache']);
+                var_dump($html);die();
+                if($html){
+                    echo $html;
+                    die();
+                }
+            }
+        }
 
     }
 
     public static function setCache(){
+        die('sss');
+        $req = Typecho_Request::getInstance();
+        $url = $req->getRequestUri();
+        
+        if(!$req->isGet()){ #仅处理GET请求
+            return;
+        }
+        if(strstr($url,'/action/') >=0 ){
+            #排除action接口,这个一般是特殊接口,所以不需要缓存
+            return;
+        }
+        
+
+        $db = Typecho_Db::get();
+        $html = ob_get_contents();
+
+        $html_gz = addslashes(gzcompress($html));
+        if(count($html_gz) >= 64 * 1024){
+            #大于64K的内容,也不能插入数据库中,存放不下。
+            return;
+        }
+        $hash = md5($url);
+        $prefix = $db->getPrefix();
+
+        $sql = "replace into {$prefix}hpstatic_cache (`hash`,`cache`) values('$hash','$html_gz')";
+        $db->query($sql); #仅插入数据库,不判断成功或者失败
 
     }
 
